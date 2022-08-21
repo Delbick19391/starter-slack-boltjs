f96a21b9b4008daaa6224abb893dc96605b8a47a
Delbick19391 <111655140+Delbick19391@users.noreply.github.com>
2022-08-21 22:18:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,69 +1,20 @@
-const express = require('express')
-const { App, ExpressReceiver } = require('@slack/bolt');
+const mineflayer = require('mineflayer');
+function createBot()
+{
+  let bot = mineflayer.createBot({
+    host: '46.4.94.213',
+    username: "SlimeShield_BOT",
+    port: 38302,
+    version: "1.18.2"
+        })
+        
+        bot.on('error', (err) => console.log(err))
+        bot.on('end', createBot)
+    
+        bot.once('spawn', () =>{
+            bot.chat("/login dk19kaZo3mA")
+        })
+    
+    }
+    createBot()
 
-const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
-
-receiver.router.use(express.static('public'))
-
-const app = new App({
-  receiver,
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-});
-
-
-app.message(':wave:', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say({
-    blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
-          },
-          "action_id": "button_click"
-        }
-      }
-    ],
-    text: `Hey there <@${message.user}>!`,
-    thread_ts: message.ts
-  });
-});
-
-app.event('app_mention', async ({ event, say }) => {
-  await say({
-    text: `Hello <@${event.user}>: If you send me a :wave: I'll send you a button to click. If you add a reaction to a message I'll say thanks.`,
-    thread_ts: event.ts
-  })
-});
-
-app.event('reaction_added', async ({ event, say }) => {
-  await say({
-    text: `Thanks for the :${event.reaction}:`,
-    thread_ts: event.item.ts
-  })
-});
-
-app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  // console.log(JSON.stringify(body,null,2))
-  await say({
-    text: `<@${body.user.id}> you clicked the button. Well done.`,
-    thread_ts: body.message.ts
-  });
-});
-
-(async () => {
-  // Start the app
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
